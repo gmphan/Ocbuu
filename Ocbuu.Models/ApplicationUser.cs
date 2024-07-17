a114b75cdafffcebce046501981b5ec9b9a6053d
Giang Phan <GiangPhan@Clayton.edu>
2024-07-17 01:03:27 -0400

    Modified page theme.

---
+++ Ocbuu.Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Ocbuu.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // Additional properties can be added here
+    }
+}
